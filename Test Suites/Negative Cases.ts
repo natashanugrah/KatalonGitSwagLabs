56d98b1bd353cefb6ddc5f1f499209f935cfb08e
natashanugrah <natasha.anugrah@binus.ac.id>
2025-01-21 11:17:39 +0700

    add new test suite: negative cases -- swag labs v1.2

---
+++ Test Suites/Negative Cases.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Negative Cases</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ad281dc9-a426-450a-aac1-0bced0920ae5</testSuiteGuid>
+</TestSuiteEntity>
